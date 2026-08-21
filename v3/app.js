b01ef631bdcbf2a53765e7878ee522cdd039509e
konxq <7r5nr429fb@privaterelay.appleid.com>
2026-08-21 09:19:26 +0200

    chore(frontend): add Financebot 3.0 frontend bootstrap

---
+++ v3/app.js
@@ -0,0 +1,10 @@
+/* Financebot 3.0 frontend bootstrap */
+
+const FinancebotApp = {
+  version: '3.0',
+  currentPage: 'home',
+};
+
+window.FinancebotApp = FinancebotApp;
+
+console.info('Financebot 3.0 frontend architecture loaded');
